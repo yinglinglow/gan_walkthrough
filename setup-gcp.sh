f3eb7ac7c9fa277cdbc4c53d2a1545f283497d4e
Ying Ling <low.yingling@gmail.com>
2018-02-18 10:25:20 +0800

    include link

--- setup-gcp.sh
+++ setup-gcp.sh
@@ -5,6 +5,7 @@ Anaconda3, Jupyter Notebook, cuda 9.1, Tensorflow, Keras, and AWS CLI
 Base code credits to https://github.com/joannasys
 
 To run:
+Setup following instructions here: https://medium.com/@howkhang/ultimate-guide-to-setting-up-a-google-cloud-machine-for-fast-ai-version-2-f374208be43
 Copy and paste the entire chunk below into your remote terminal on startup.
 After everything runs (~5minutes) there is a reboot at the end of the script (necessary)
 
